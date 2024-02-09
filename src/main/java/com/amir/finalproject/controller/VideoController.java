30de3980330b635c2965d5b6e542f3ff4ea2d83a
amirholya01 <120662275+amirhosseinolya@users.noreply.github.com>
2024-02-09 17:58:35 +0100

    download file

--- src/main/java/com/amir/finalproject/controller/VideoController.java
+++ src/main/java/com/amir/finalproject/controller/VideoController.java
@@ -2,11 +2,15 @@ package com.amir.finalproject.controller;
 
 import com.amir.finalproject.model.Video;
 import com.amir.finalproject.service.VideoService;
+import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.Resource;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("video")
@@ -21,4 +25,20 @@ public class VideoController {
     }
 
 
+    //Get all video
+    @GetMapping
+    public List<Video> videos(){
+        return videoService.findAllVideos();
+    }
+
+    //Get a video by id
+    @GetMapping("{id}")
+    public Video findById(@PathVariable("id") Long id){
+        return videoService.findById(id);
+    }
+
+    @GetMapping("/download/{id:.+}") //everything after id
+    public ResponseEntity<Resource> downloadFile(@PathVariable Long id, HttpServletRequest request){
+        return (ResponseEntity<Resource>) videoService.downloadFile(id, request);
+    }
 }
--- src/main/java/com/amir/finalproject/service/VideoService.java
+++ src/main/java/com/amir/finalproject/service/VideoService.java
@@ -2,12 +2,19 @@ package com.amir.finalproject.service;
 
 import com.amir.finalproject.model.Video;
 import com.amir.finalproject.repository.VideoRepository;
+import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.Resource;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
-import org.springframework.util.StringUtils;
 import org.springframework.web.multipart.MultipartFile;
 
+import javax.imageio.IIOException;
+import java.io.IOException;
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class VideoService {
@@ -27,6 +34,39 @@ public class VideoService {
         return video;
     }
 
+    //Get all videos
+    public List<Video> findAllVideos(){
+        return videoRepository.findAll();
+    }
+
+    //Get a video by id
+    public Video findById(Long id){
+        Optional<Video> video = videoRepository.findById(id);
+        return video.isPresent() ? video.get() : null; //ternary operator in Java
+    }
 
 
+    //Download file
+    public Resource downloadFile(Long id, HttpServletRequest request){
+        Optional<Video> video = videoRepository.findById(id);
+        Resource resource = fileStorageService.loadFileAsResource(video.get().getUrl());
+
+        String contentType = null;
+
+        try {
+            contentType = request.getServletContext().getMimeType(resource.getFile().getAbsolutePath());
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        if(contentType == null){
+            contentType = "application/octet-stream";
+        }
+
+
+        return ResponseEntity.ok()
+                .contentType(MediaType.parseMediaType(contentType))
+                .header(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=\"", resource.getFilename() + "\"")
+                .body(resource).getBody();
+
+    }
 }
